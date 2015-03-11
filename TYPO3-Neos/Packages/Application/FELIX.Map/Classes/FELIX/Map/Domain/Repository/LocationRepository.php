6d1756447f9cb6298eb8fb2961672aa0b6a171d1
Felix <fische@courages.net>
2015-03-11 11:55:30 +0100

    flow-package erstellt und ein wenig angepasst (noch keine db-operationen über doctrine:update oder migrations)

---
+++ TYPO3-Neos/Packages/Application/FELIX.Map/Classes/FELIX/Map/Domain/Repository/LocationRepository.php
@@ -0,0 +1,19 @@
+<?php
+namespace FELIX\Map\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "FELIX.Map".             *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Persistence\Repository;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class LocationRepository extends Repository {
+
+	// add customized methods here
+
+}

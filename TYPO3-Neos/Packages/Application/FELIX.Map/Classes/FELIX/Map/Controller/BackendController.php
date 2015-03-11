1a32075e590a2a17d90bfe40dc9b526eb93dc1a2
Felix <fische@courages.net>
2015-03-11 13:12:40 +0100

    added backend module as submodule of management

---
+++ TYPO3-Neos/Packages/Application/FELIX.Map/Classes/FELIX/Map/Controller/BackendController.php
@@ -0,0 +1,25 @@
+<?php
+namespace FELIX\Map\Controller;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "FELIX.Map".             *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * The TYPO3 User Settings module controller
+ *
+ * @Flow\Scope("singleton")
+ */
+class BackendController extends \TYPO3\Neos\Controller\Module\AbstractModuleController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+
+	}
+
+}

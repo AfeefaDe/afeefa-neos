c829d5ec4e3287f5c8eef8e0f3462d99f9c236d4
Felix <fische@courages.net>
2015-03-16 21:25:44 +0100

    location backend module created (only skeleton so far)

---
+++ TYPO3-Neos/Packages/Application/DDFA.Map/Classes/DDFA/Map/Controller/Module/DDFA/LocationsModuleController.php
@@ -0,0 +1,29 @@
+<?php
+namespace DDFA\Map\Controller\Module\DDFA;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "DDFA.Map".             *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * The TYPO3 User Settings module controller
+ *
+ * @Flow\Scope("singleton")
+ */
+class LocationsModuleController extends \TYPO3\Neos\Controller\Module\AbstractModuleController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+
+	}
+
+	public function addAction() {
+
+	}
+
+}

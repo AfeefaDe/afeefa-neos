dddc0910bf80d493f3f12e6e3583f29ce3403fa2
foobar0112 <foobar2@posteo.de>
2015-04-26 17:49:30 +0200

    changed routes of API plugin

---
+++ TYPO3-Neos/Packages/Application/DDFA.Map/Classes/DDFA/Map/Controller/Plugin/DDFA/LocationAPIController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DDFA\Map\Controller\Plugin\DDFA;
+
+
+use TYPO3\Flow\Mvc\Controller\ActionController;
+
+class LocationAPIController extends ActionController {
+
+    public function getAction() {
+
+    }
+}

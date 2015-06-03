14887364d556664a7c03b91504ba7dea94e523c2
Felix <fische@courages.net>
2015-06-03 10:33:07 +0200

    fixed safari bug #108

--- TYPO3-Neos/Packages/Sites/DDFA.dresdenfueralleDe/Resources/Public/DDFA/js/LanguageManager.js
+++ TYPO3-Neos/Packages/Sites/DDFA.dresdenfueralleDe/Resources/Public/DDFA/js/LanguageManager.js
@@ -18,10 +18,11 @@ qx.Class.define("LanguageManager", {
         init: function( cb ){
             var that = this;
 
-            if( _.contains(APP.getConfig().languages, navigator.language) )
-                that.setCurrentLang( navigator.language );
+            var browserLang = navigator.language.split('-')[0];
+            if( _.contains( APP.getConfig().languages, browserLang) )
+                that.setCurrentLang( browserLang );
             else
-                that.setCurrentLang( that.getConfig().languages[0] );
+                that.setCurrentLang( APP.getConfig().languages[0] );
 
             $('body').addClass(that.getCurrentLang());
             

06e63dc3383d7152b07a357970f1cfffae492986
Felix <fische@courages.net>
2017-02-06 22:08:15 +0100

    basic working implementation

--- neos/Packages/Sites/DDFA.dresdenfueralleDe/Resources/Public/DDFA/js/Views/LanguageView.js
+++ neos/Packages/Sites/DDFA.dresdenfueralleDe/Resources/Public/DDFA/js/Views/LanguageView.js
@@ -18,63 +18,73 @@ qx.Class.define("LanguageView", {
 			that.view = $("<div />");
 			that.view.attr('id', that.getViewId());
 
-			that.buttons = [];
+			// that.buttons = [];
 
-			// add root button
-			// that.rootBtn = $("<div />");
-			// that.rootBtn.addClass('btn root-btn ' + APP.getLM().getCurrentLang() );
-			// that.view.append(that.rootBtn);
-			// that.buttons.push(that.rootBtn);
-
-			// add other language buttons
-			// var remainingLanguages = _.without( APP.getConfig().languages, APP.getLM().getCurrentLang() );
-			// _.each( remainingLanguages, function(lang){
-			_.each( APP.getConfig().languages, function(lang){
-				var langBtn = $("<div />");
-				langBtn.addClass('btn ' + lang);
-
-				// tooltip
-				that.createTooltip(
-					langBtn, 
-					function(){
-						return that.getWording('lan.' + lang, lang) + ' (' + that.getWording('lan.' + lang) + ')';
-					}(),
-					'hover',
-					'top',
-					'desktop'					
-				);
-
-				langBtn.click(function(){
-					APP.getDataManager().getUITranslations(lang, function(data){
-            APP.getLM().setBib(data);
-						that.say('languageChanged', lang);
-					});
+			// circle button to open
+			var langBtn = $("<div />")
+				.addClass(APP.getLM().getCurrentLang())
+				.attr('id', 'lang-btn')
+				.click(function(){
+					that.open();
 				});
 
-				that.buttons.push( {el: langBtn, lang: lang} );
+			that.view.append(langBtn);
 
-				that.view.append(langBtn);
+			// list container
+			that.listContainer = $("<div />")
+				.attr('id', 'list-container')
+				.append( $("<ul />") );
+			
+			// list
+			that.list = $("<ul />");
+
+			that.listContainer.append(that.list);
+			that.view.append(that.listContainer);
+
+			// all the languages as list items
+			_.each( APP.getConfig().languages, function(lang){
+				var langItem = $("<li />")
+					.addClass(lang)
+					.append(that.getWording('lan.'+lang))
+					.click(function(){
+						that.close();
+
+						APP.getDataManager().getUITranslations(lang, function(data){
+	            APP.getLM().setBib(data);
+							that.say('languageChanged', lang);
+						});
+					});
+
+				// that.buttons.push( {el: langItem, lang: lang} );
+
+				that.list.append(langItem);
 			});
 			
 			$('#main-container').append(that.view);
 
 			this.base(arguments);
-
-			that.load();
 		},
 
-		load: function(){
+		open: function(){
 			var that = this;
 
-			_.each( that.buttons, function(btn){
-				if( btn.el.hasClass( APP.getLM().getCurrentLang() ) )
-					btn.el.addClass('active');
+			that.view.addClass('active');
+			that.showCurtain(true);
+			
+			// highlight current language
+			_.each( that.listItems, function(item){
+				if( item.el.hasClass( APP.getLM().getCurrentLang() ) )
+					item.el.addClass('active');
 			});
 
 		},
 
 		addEvents: function(){
 			var that = this;
+
+			// that.listen('curtainclicked', function(){
+   //      	if( that.view.hasClass('active') ) that.close();
+  	// 	});
 
 			that.listen('LanguageViewRendered', function(){
 				// alert('Willkommen auf Afeefa.de! Wähle deine Sprache:');
@@ -89,25 +99,24 @@ qx.Class.define("LanguageView", {
 			var that = this;
 
 			// reset all buttons
-			_.each( that.buttons, function(btn) {
-				btn.el.removeClass('active');
-			});
+			// _.each( that.buttons, function(btn) {
+			// 	btn.el.removeClass('active');
+			// });
 
 		},
 
 		changeLanguage: function(){
 			var that = this;
 
-			that.reset();
-			that.load();
+			// that.reset();
+			// that.load();
 		},
 
 		close: function(){
 			var that = this;
-
-			// TODO: only do in mobile version
-			// that.addRequestBtn.css('display', 'none');
-			// that.addOfferBtn.css('display', 'none');
+			
+			that.view.removeClass('active');
+			that.showCurtain(false);
 		}
 	}
 
